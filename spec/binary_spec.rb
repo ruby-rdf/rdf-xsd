c45d7bc4d9f22c02bb6d2dc63cae7c9e630836e2
Gregg Kellogg <gregg@kellogg-assoc.com>
2015-07-09 14:32:50 -0400

    Skip HexBinary specs on JRuby and RBX due to validation issues.

--- spec/binary_spec.rb
+++ spec/binary_spec.rb
@@ -7,7 +7,7 @@ describe RDF::Literal do
   include_examples 'RDF::Literal lookup',
                    { RDF::XSD.base64Binary => RDF::Literal::Base64Binary }
 
-  describe RDF::Literal::HexBinary do
+  describe RDF::Literal::HexBinary, pending: ("RBX and JRuby validation issues)" if %w(jruby rbx).include?(RUBY_ENGINE)) do
     it_behaves_like 'RDF::Literal with datatype and grammar',
                     '3f3c6d78206c657673726F693D6E3122302e20226E656F636964676e223D54552d4622383E3f',
                     described_class::DATATYPE.to_s
