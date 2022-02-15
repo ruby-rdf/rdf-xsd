a05617fab7d60f6d7990bc0051853b5d8a65e97d
Gregg Kellogg <gregg@greggkellogg.net>
2022-02-15 12:14:32 -0800

    Update Duration to be more compliant with XML Schema definition. * Rework `#to_f` using time/datetime calculations. * Adds `#==`, and `#<` and `#>` for sub-types.

--- lib/rdf/xsd/duration.rb
+++ lib/rdf/xsd/duration.rb
@@ -7,6 +7,7 @@ module RDF; class Literal
   #
   # @see   https://www.w3.org/TR/xmlschema11-2/#duration
   class Duration < Literal
+    TIME0 = ::Time.new(0)
     DATATYPE = RDF::XSD.duration
     GRAMMAR  = %r(\A
       (?<si>-)?
@@ -14,12 +15,12 @@ module RDF; class Literal
           |  (?:(?:(?<mo>\d+)M)(?:(?<da>\d+)D)?)
           |  (?:(?<da>\d+)D)
           )
-          (?:T(?:(?:(?:(?<hr>\d+)H)(?:(?<mi>\d+)M)?(?<se>\d+(?:\.\d+)?S)?)
+          (?:T(?:(?:(?:(?<hr>\d+)H)(?:(?<mi>\d+)M)?(?:(?<se>\d+(?:\.\d+)?)S)?)
               |  (?:(?:(?<mi>\d+)M)(?:(?<se>\d+(?:\.\d+)?)S)?)
               |  (?:(?<se>\d+(?:\.\d+)?)S)
               )
           )?
-       |(?:T(?:(?:(?:(?<hr>\d+)H)(?:(?<mi>\d+)M)?(?<se>\d+(?:\.\d+)?S)?)
+       |(?:T(?:(?:(?:(?<hr>\d+)H)(?:(?<mi>\d+)M)?(?:(?<se>\d+(?:\.\d+)?)S)?)
             |  (?:(?:(?<mi>\d+)M)(?:(?<se>\d+(?:\.\d+)?)S)?)
             |   (?:(?<se>\d+(?:\.\d+)?)S)
             )
@@ -28,26 +29,34 @@ module RDF; class Literal
     \z)x.freeze
 
     ##
-    # * Given a Numeric, assumes that it is milliseconds
-    # * Given a String, parse as xsd:duration
-    # * Hash form is used for internal representation
+    # * Given a Numeric, assumes that it is milliseconds with no month component.
+    # * Given a String, parse as xsd:duration into months and seconds
+    # * Given a Hash containing any of `:yr`, `:mo`, :da`, `:hr`, `:mi` or `:si`, it is transformed into months and seconds
+    # * Internal representation is the `tuple(months, secons)`
     # @param  [Duration, Hash, Numeric, #to_s] value
     # @option options [String] :lexical (nil)
     def initialize(value, datatype: nil, lexical: nil, **options)
       super
       @object   = case value
       when Hash
-        value = value.dup
-        value[:yr] ||= value[:years]
-        value[:mo] ||= value[:months]
-        value[:da] ||= value[:days]
-        value[:hr] ||= value[:hours]
-        value[:mi] ||= value[:minutes]
-        value[:se] ||= value[:seconds]
-        
-        value
-      when Duration, Numeric
-        {:se => value.to_f}
+        months = value[:yr].to_i * 12 + value[:mo].to_i
+        seconds = value[:da].to_i * 3600 * 24 +
+                  value[:hr].to_i * 3600 +
+                  value[:mi].to_i * 60 +
+                  value[:se].to_f
+
+        if value[:si]
+          if months != 0
+            months = -months
+          else
+            seconds = -seconds
+          end
+        end
+        [months, seconds]
+      when Duration
+        value.object
+      when Numeric
+        [0, value]
       else
         parse(value.to_s)
       end
@@ -61,33 +70,8 @@ module RDF; class Literal
     # @return [RDF::Literal] `self`
     # @see    https://www.w3.org/TR/xmlschema11-2/#dateTime
     def canonicalize!
-      @string = @humanize = nil
-      if @object[:se].to_i > 60
-        m_r = (@object[:se].to_f / 60) - 1
-        @object[:se] -=  m_r * 60
-        @object[:mi] = @object[:mi].to_i + m_r
-      end
-      if @object[:mi].to_i > 60
-        h_r = (@object[:mi].to_i / 60) - 1
-        @object[:mi] -=  h_r * 60
-        @object[:hr] = @object[:hr].to_i +  h_r
-      end
-      if @object[:hr].to_i > 24
-        d_r = (@object[:hr].to_i / 24) - 1
-        @object[:hr] -=  d_r * 24
-        @object[:da] = @object[:da].to_i + d_r
-      end
-      if @object[:da].to_i > 30
-        m_r = (@object[:da].to_i / 30) - 1
-        @object[:da] -=  m_r * 30
-        @object[:mo] = @object[:mo].to_i + m_r
-      end
-      if @object[:mo].to_i > 12
-        y_r = (@object[:mo].to_i / 12) - 1
-        @object[:mo] -=  y_r * 12
-        @object[:yr] = @object[:yr].to_i + y_r
-      end
-      @object.to_s  # side-effect
+      @string = @humanize = @hash = nil
+      self.to_s  # side-effect
       self
     end
 
@@ -99,7 +83,23 @@ module RDF; class Literal
     #
     # @return [Boolean]
     def valid?
-      !!(value =~ GRAMMAR)
+      !!value.match?(self.class.const_get(:GRAMMAR))
+    end
+
+    ##
+    # Returns a hash representation.
+    #
+    # @return [Hash]
+    def to_hash
+      @hash ||= {
+        si: ('-' if @object.compact.first < 0),
+        yr: (@object.first.abs / 12),
+        mo: (@object.first.abs % 12),
+        da: (@object.last.abs.to_i / (3600 * 24)),
+        hr: (@object.last.abs.to_i % (3600 * 24) / 3600),
+        mi: (@object.last.abs.to_i % 3600 / 60),
+        se: (@object.last % 60)
+      }
     end
 
     ##
@@ -108,14 +108,16 @@ module RDF; class Literal
     # @return [String]
     def to_s
       @string ||= begin
-        str = @object[:si] == '-' ? "-P" : "P"
-        str << "%dY" % @object[:yr].to_i if @object[:yr]
-        str << "%dM" % @object[:mo].to_i if @object[:mo]
-        str << "%dD" % @object[:da].to_i if @object[:da]
-        str << "T" if @object[:hr] || @object[:mi] || @object[:se]
-        str << "%dH" % @object[:hr].to_i if @object[:hr]
-        str << "%dM" % @object[:mi].to_i if @object[:mi]
-        str << "#{sec_str}S" if @object[:se]
+        hash = to_hash
+        str = @object.compact.first < 0 ? '-P' : 'P'
+        hash = to_hash
+        str << "%dY" % hash[:yr] if hash[:yr] > 0
+        str << "%dM" % hash[:mo] if hash[:mo] > 0
+        str << "%dD" % hash[:da] if hash[:da] > 0
+        str << "T" if (hash[:hr] + hash[:mi] + hash[:se]) > 0
+        str << "%dH" % hash[:hr] if hash[:hr] > 0
+        str << "%dM" % hash[:mi] if hash[:mi] > 0
+        str << sec_str + 'S' if hash[:se] > 0
       end
     end
 
@@ -129,23 +131,27 @@ module RDF; class Literal
       @humanize ||= {}
       @humanize[lang] ||= begin
         # Just english, for now
+        return "Invalid duration #{value.to_s.inspect}" unless valid?
+
+        md = value.match(GRAMMAR)
         ar = []
-        ar << plural(@object[:yr], "year")
-        ar << plural(@object[:mo], "month")
-        ar << plural(@object[:da], "day")
-        ar << plural(@object[:hr], "hour")
-        ar << plural(@object[:mi], "minute")
-        ar << plural(sec_str, "second") if @object[:se]
-        ar = ar.compact
+        ar << plural(md[:yr], "year") if md[:yr]
+        ar << plural(md[:mo], "month") if md[:mo]
+        ar << plural(md[:da], "day") if md[:da]
+        ar << plural(md[:hr], "hour") if md[:hr]
+        ar << plural(md[:mi], "minute") if md[:mi]
+        ar << plural(md[:se], "second") if md[:se]
         last = ar.pop
         first = ar.join(" ")
         res = first.empty? ? last : "#{first} and #{last}"
-        @object[:si] == '-' ? "#{res} ago" : res
+        md[:si] == '-' ? "#{res} ago" : res
       end
     end
 
     ##
-    # Equal compares as DateTime objects
+    # Returns `true` if `self` and `other` are durations of the same length.
+    #
+    # @see https://www.w3.org/TR/xpath-functions-31/#func-duration-equal
     def ==(other)
       # If lexically invalid, use regular literal testing
       return super unless self.valid?
@@ -153,9 +159,9 @@ module RDF; class Literal
       case other
       when Duration
         return super unless other.valid?
-        self.to_f == other.to_f
+        @object == other.object
       when String
-        self.to_s(:xml) == other
+        self.to_s == other
       when Numeric
         self.to_f == other
       when Literal::DateTime, Literal::Time, Literal::Date
@@ -165,21 +171,18 @@ module RDF; class Literal
       end
     end
 
+    # Return a floating point representation of this duration.
+    #
+    # Transformation is done by representing adding the seconds trait to `Time.new(0)`, transforming to `DateTime` and applying the months trait.
     # @return [Float]
     def to_f
-      ( @object[:yr].to_i * 365 * 24 * 3600 +
-        @object[:mo].to_i * 30 * 24 * 3600 +
-        @object[:da].to_i * 24 * 3600 +
-        @object[:hr].to_i * 3600 +
-        @object[:mi].to_i * 60 +
-        @object[:se].to_f
-      ) * (@object[:si] == '-' ? -1 : 1)
+      ((TIME0 + @object.last).to_datetime >> @object.first).to_time - TIME0
     end
 
     # @return [Integer]
     def to_i; Integer(self.to_f); end
     
-    private
+  private
     # Reverse convert from XSD version of duration
     # XSD allows -P1111Y22M33DT44H55M66.666S with any combination in regular order
     # We assume 1M == 30D, but are out of spec in this regard
@@ -188,11 +191,27 @@ module RDF; class Literal
     # @param [String] value XSD formatted duration
     # @return [Duration]
     def parse(value)
-      value.to_s.match(GRAMMAR)
+      return [0, 0] unless md = value.to_s.match(GRAMMAR)
+
+      months  = md[:yr].to_i * 12 + md[:mo].to_i
+      seconds = md[:da].to_i * 3600 * 24 +
+                md[:hr].to_i * 3600 +
+                md[:mi].to_i * 60 +
+                md[:se].to_f
+
+      if md[:si]
+        if months != 0
+          months = -months
+        else
+          seconds = -seconds
+        end
+      end
+
+      [months, seconds]
     end
     
     def sec_str
-      sec = @object[:se].to_f
+      sec = @object.last % 60
       ((sec.truncate == sec ? "%d" : "%2.3f") % sec).sub(/(\.[1-9]+)0+$/, '\1')
     end
   end # Duration
@@ -203,7 +222,7 @@ module RDF; class Literal
   # dayTimeDuration is a datatype ·derived· from duration by restricting its ·lexical representations· to instances of dayTimeDurationLexicalRep. The ·value space· of dayTimeDuration is therefore that of duration restricted to those whose ·months· property is 0.  This results in a duration datatype which is totally ordered.
   #
   # @see   https://www.w3.org/TR/xmlschema11-2/#dayTimeDuration
-  class DayTimeDuration < Literal
+  class DayTimeDuration < Duration
     DATATYPE = RDF::XSD.dayTimeDuration
     GRAMMAR  = %r(\A
       (?<si>-)?
@@ -221,6 +240,72 @@ module RDF; class Literal
         )
        )
     \z)x.freeze
+
+    ##
+    # Returns `true` if less than other for defined datatypes
+    #
+    # @see https://www.w3.org/TR/xpath-functions-31/#func-dayTimeDuration-less-than
+    def <(other)
+      # If lexically invalid, use regular literal testing
+      return super unless self.valid?
+
+      case other
+      when Duration
+        return super unless other.valid?
+        @object.last < other.object.last
+      when Numeric
+        self.last < other
+      when Literal::DateTime, Literal::Time, Literal::Date
+        false
+      else
+        super
+      end
+    end
+
+    ##
+    # Returns `true` if greater than other for defined datatypes
+    #
+    # @see https://www.w3.org/TR/xpath-functions-31/#func-yearMonthDuration-less-than
+    def >(other)
+      # If lexically invalid, use regular literal testing
+      return super unless self.valid?
+
+      case other
+      when Duration
+        return super unless other.valid?
+        @object.last > other.object.first
+      when Numeric
+        self.last > other
+      when Literal::DateTime, Literal::Time, Literal::Date
+        false
+      else
+        super
+      end
+    end
+
+    # Years
+    # @return [Integer]
+    def years; to_hash[:yr].to_i * (to_hash[:si] ? -1 : 0); end
+
+    # Months
+    # @return [Integer]
+    def months; to_hash[:mo].to_i * (to_hash[:si] ? -1 : 0); end
+
+    # Days
+    # @return [Integer]
+    def days; to_hash[:da].to_i * (to_hash[:si] ? -1 : 0); end
+
+    # Hours
+    # @return [Integer]
+    def hours; to_hash[:hr].to_i * (to_hash[:si] ? -1 : 0); end
+
+    # Minutes
+    # @return [Integer]
+    def minutes; to_hash[:mi].to_i * (to_hash[:si] ? -1 : 0); end
+
+    # Seconds
+    # @return [Integer]
+    def seconds; to_hash[:se].to_i * (to_hash[:si] ? -1 : 0); end
   end # DayTimeDuration
 
   ##
@@ -229,7 +314,7 @@ module RDF; class Literal
   # yearMonthDuration is a datatype ·derived· from duration by restricting its ·lexical representations· to instances of yearMonthDurationLexicalRep.  The ·value space· of yearMonthDuration is therefore that of duration restricted to those whose ·seconds· property is 0.  This results in a duration datatype which is totally ordered.
   #
   # @see   https://www.w3.org/TR/xmlschema11-2/#yearMonthDuration
-  class YearMonthDuration < Literal
+  class YearMonthDuration < Duration
     DATATYPE = RDF::XSD.yearMonthDuration
     GRAMMAR  = %r(\A
       (?<si>-)?
@@ -238,5 +323,47 @@ module RDF; class Literal
           )
        )
     \z)x.freeze
-  end # DayTimeDuration
+
+    ##
+    # Returns `true` if less than other for defined datatypes
+    #
+    # @see https://www.w3.org/TR/xpath-functions-31/#func-yearMonthDuration-less-than
+    def <(other)
+      # If lexically invalid, use regular literal testing
+      return super unless self.valid?
+
+      case other
+      when Duration
+        return super unless other.valid?
+        @object.first < other.object.first
+      when Numeric
+        self.first < other
+      when Literal::DateTime, Literal::Time, Literal::Date
+        false
+      else
+        super
+      end
+    end
+
+    ##
+    # Returns `true` if greater than other for defined datatypes
+    #
+    # @see https://www.w3.org/TR/xpath-functions-31/#func-yearMonthDuration-less-than
+    def >(other)
+      # If lexically invalid, use regular literal testing
+      return super unless self.valid?
+
+      case other
+      when Duration
+        return super unless other.valid?
+        @object.first > other.object.first
+      when Numeric
+        self.first > other
+      when Literal::DateTime, Literal::Time, Literal::Date
+        false
+      else
+        super
+      end
+    end
+  end # YearMonthDuration
 end; end # RDF::Literal
--- spec/duration_spec.rb
+++ spec/duration_spec.rb
@@ -2,9 +2,18 @@ $:.unshift "."
 require 'spec_helper'
 
 describe RDF::Literal::Duration do
+  include_examples 'RDF::Literal with datatype and grammar', "P2Y6M5DT12H35M30S",
+                   described_class::DATATYPE.to_s
+  include_examples 'RDF::Literal with datatype and grammar', "P2Y6M5DT12H35M30S",
+                   described_class::DATATYPE.to_s
+  include_examples 'RDF::Literal lexical values', "P2Y6M5DT12H35M30S"
+
+  include_examples 'RDF::Literal lookup',
+                   { described_class::DATATYPE => described_class }
+
   describe "initialize" do
     it "creates given a Hash" do
-      expect(described_class.new({seconds: 10, minutes: 1}, **{}).to_i).to eq 70
+      expect(described_class.new({se: 10, mi: 1})).to eq described_class.new('PT70S')
     end
   end
 
@@ -15,28 +24,52 @@ describe RDF::Literal::Duration do
   describe "#to_f" do
     {
       "PT130S"                    => 130,
-      "PT130M"                    => 130*60,
+      "PT130M"                    => 7800.0,
       "PT130H"                    => 130*3600,
       "P130D"                     => 130*24*3600,
-      "P130M"                     => 130*30*24*3600,
-      "P130Y"                     => 130*365*24*3600,
+      "P130M"                     => 341884800.0,
+      "P130Y"                     => 4102444800.0,
       "PT2M10S"                   => (2*60+10),
       "P1DT2S"                    => (1*3600*24+2),
       "P1DT2H"                    => 26*3600,
-      "P0Y20M0D"                  => 20*30*24*3600,
+      "P0Y20M0D"                  => 52617600.0,
       "P0Y"                       => 0,
-      "-P60D"                     => -60*24*3600,
+      "-P60D"                     => -5184000.0,
       "PT1M30.5S"                 => 60+30.5,
-      "P20M"                      => 20*30*24*3600,
+      "P20M"                      => 52617600.0,
       "PT20M"                     => 20*60,
       "-P1Y"                      => (-365*3600*24),
       "PT1004199059S"             => 1004199059,
       "P1Y2M3DT5H20M30.123S"      => ((365+60+3)*3600*24+5*3600+20*60+30.123),
-      "-P1111Y11M23DT4H55M16.666S"=> -((((1111*365+11*30+23)*24)+4)*3600+55*60+16.666),
-      "P2Y6M5DT12H35M30S"         => (((2*365+6*30+5)*24)+12)*3600+35*60+30,
+      "-P1111Y11M23DT4H55M16.666S"=> -35086590283.334,
+      "P2Y6M5DT12H35M30S"         => 79274130.0,
     }.each do |s, f|
       it "parses #{s}" do
         expect(described_class.new(s).to_f).to eq f
+      end
+    end
+  end
+
+  describe "#==" do
+    {
+      [RDF::Literal::Duration.new("P1Y"), RDF::Literal::Duration.new("P12M")] => true,
+      [RDF::Literal::Duration.new("PT24H"), RDF::Literal::Duration.new("P1D")] => true,
+      [RDF::Literal::Duration.new("P1Y"), RDF::Literal::Duration.new("P365D")] => false,
+      [RDF::Literal::YearMonthDuration.new("P0Y"), RDF::Literal::DayTimeDuration.new("P0D")] => true,
+      [RDF::Literal::YearMonthDuration.new("P1Y"), RDF::Literal::DayTimeDuration.new("P365D")] => false,
+      [RDF::Literal::YearMonthDuration.new("P2Y"), RDF::Literal::YearMonthDuration.new("P24M")] => true,
+      [RDF::Literal::DayTimeDuration.new("P10D"), RDF::Literal::DayTimeDuration.new("PT240H")] => true,
+      [RDF::Literal::Duration.new("P2Y0M0DT0H0M0S"), RDF::Literal::YearMonthDuration.new("P24M")] => true,
+      [RDF::Literal::Duration.new("P0Y0M10D"), RDF::Literal::DayTimeDuration.new("PT240H")] => true,
+    }.each do |(a, b), res|
+      if res
+        it "#{a} == #{b}" do
+          expect(a).to eq b
+        end
+      else
+        it "#{a} != #{b}" do
+          expect(a).not_to eq b
+        end
       end
     end
   end
@@ -59,7 +92,7 @@ describe RDF::Literal::Duration do
       "-P60D"                     => "60 days ago",
       "PT1M30.5S"                 => "1 minute and 30.5 seconds",
     }.each do |s, h|
-      it "produces #{h}" do
+      it "produces #{h} from #{s.inspect}" do
         expect(described_class.new(s).humanize).to eq h
       end
     end
@@ -67,9 +100,9 @@ describe RDF::Literal::Duration do
 end
 
 describe RDF::Literal::DayTimeDuration do
-  include_examples 'RDF::Literal',
-                   'PT130S',
+  include_examples 'RDF::Literal with datatype and grammar', "PT130S",
                    described_class::DATATYPE.to_s
+  include_examples 'RDF::Literal lexical values', "PT130S"
 
   include_examples 'RDF::Literal lookup',
                    { described_class::DATATYPE => described_class }
@@ -106,10 +139,9 @@ describe RDF::Literal::DayTimeDuration do
 end
 
 describe RDF::Literal::YearMonthDuration do
-
-  include_examples 'RDF::Literal',
-                   'P130M',
+  include_examples 'RDF::Literal with datatype and grammar', "P130M",
                    described_class::DATATYPE.to_s
+  include_examples 'RDF::Literal lexical values', "P130M"
 
   include_examples 'RDF::Literal lookup',
                    { described_class::DATATYPE => described_class }
